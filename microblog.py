3caf9dbc962ba14c038e9fcfa731fe542643cb72
Nash Sibanda <contact@nashsibanda.com>
2021-01-22 23:53:53 +0000

    Add context for flask shell

--- microblog.py
+++ microblog.py
@@ -1,2 +1,7 @@
 # ? Following tutorial: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
