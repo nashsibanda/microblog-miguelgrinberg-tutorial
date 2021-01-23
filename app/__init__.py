4287b9328fed8cb00dd2b0f3b13177c00b916644
Nash Sibanda <contact@nashsibanda.com>
2021-01-23 00:03:51 +0000

    Add flask_login, LoginManager and UserMixin

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+login = LoginManager(app)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
