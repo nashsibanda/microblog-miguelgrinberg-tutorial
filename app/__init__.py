962c9ed5baf94ee6c7b0be988d0340777e3d0e86
Nash Sibanda <contact@nashsibanda.com>
2021-01-23 20:36:45 +0000

    Add error handling pages

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import Config
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, migrate
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,4 +12,4 @@ login.login_view = 'login'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes, models
+from app import routes, models, errors
