1d2bcd932ad4725af3bfd40667332c80ccb1801a
Nash Sibanda <contact@nashsibanda.com>
2021-01-23 01:22:32 +0000

    Add avatars to user profile page

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from werkzeug.security import check_password_hash, generate_password_hash
-from app import db, login
 from flask_login import UserMixin
+from hashlib import md5
+from app import db, login
 
 
 class User(UserMixin, db.Model):
@@ -19,6 +20,12 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size
+        )
 
 
 @login.user_loader
