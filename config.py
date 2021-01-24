7f1c9ab279609a4f64bd5f6b83fc9022ba20a121
Nash Sibanda <contact@nashsibanda.com>
2021-01-24 22:44:41 +0000

    Add pagination links on index and profile pages

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['nashx90@gmail.com']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
