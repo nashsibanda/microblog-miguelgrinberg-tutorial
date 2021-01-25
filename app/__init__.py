8adfc42ad883270aa5580946da022b5d5a6e0e31
Nash Sibanda <contact@nashsibanda.com>
2021-01-25 23:11:44 +0000

    Add bootstrap, form macros and refactor content blocks

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -13,6 +14,7 @@ app.config.from_object(Config)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
