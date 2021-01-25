e27a1c757c610e1ada11313c10a8f962bb328f02
Nash Sibanda <contact@nashsibanda.com>
2021-01-25 17:52:17 +0000

    Add email functionality and framework

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
 import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -11,6 +12,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
