8708a775909f476be42e0001da50b493a5ea52c3
Nash Sibanda <contact@nashsibanda.com>
2021-01-31 23:49:03 +0000

    Refactor error handling into blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -54,4 +54,7 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
 
-from app import routes, models, errors
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
+
+from app import routes, models
